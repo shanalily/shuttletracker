042f6102c2d5563a472902e4fbf1c47f0d39b6d1
Thomas Shweh <shweht@rpi.edu>
2018-11-04 19:45:49 -0500

    updated vendor, pls run govendor sync

--- api/schedules.go
+++ api/schedules.go
@@ -3,9 +3,12 @@ package api
 import (
 	// "encoding/json"
 	"net/http"
+	"fmt"
 
+  "github.com/Luxurioust/excelize"
 	// "github.com/wtg/shuttletracker"
 	"github.com/wtg/shuttletracker/log"
+	"testing"
 )
 
 func (api *API) SchedulesHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +19,21 @@ func (api *API) SchedulesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	WriteJSON(w, schedules)
+}
+
+func testParsing(t *testing.T){
+	xlsx, err := excelize.OpenFile("2018-19 M-Fri Campus Schedule East-West shuttle schedule.xlsx")
+	    if err != nil {
+	        fmt.Println(err)
+	        return
+	    }
+
+	    // Get all the rows in the Sheet1.
+	    rows := xlsx.GetRows("East Master")
+	    for _, row := range rows {
+	        for _, colCell := range row {
+	            fmt.Print(colCell, "\t")
+	        }
+	        fmt.Println()
+	    }
 }
