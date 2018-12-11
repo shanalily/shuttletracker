19db3c748814ee69b1d55567774f86c6127ba0c4
shwehtom89 <shweht@rpi.edu>
2018-12-11 01:40:25 -0500

    added flags for Cobra Commands

---
+++ cmd/schedule.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/wtg/shuttletracker/schedule"
+    "github.com/spf13/cobra"
+)
+
+func init(){
+	scheduleCmd.Flags().String("link", "https://rpi.box.com/shared/static/naf8gm1wjdor8tbebho5k0t28wksaygd.xlsx", "linked file")
+	rootCmd.AddCommand(scheduleCmd)
+}
+// actual command 
+var scheduleCmd = &cobra.Command{
+	Use:   "schedule",
+	Long:  "Add schedules based on a user specified link",
+	Run: func(cmd *cobra.Command, args []string) {
+		link := cmd.Flag("link").Value.String()
+		schedule.ReadLink(link)
+	},
+}
